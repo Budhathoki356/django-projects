62d14e9f7bec76ae1f4114e401201e2f66c8a554
budhathoki356 <budhathoki1418@gmail.com>
2023-07-31 18:44:37 +0545

    feat: Add templates

--- blog-app/blog_app/blog/views.py
+++ blog-app/blog_app/blog/views.py
@@ -1,8 +1,39 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Eklal Budhathoki',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2023'
+    },
+    {
+        'author': 'Aziz Hussen',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 27, 2023'
+    },
+    {
+        'author': 'Dhoni Sharma',
+        'title': 'Blog Post 3',
+        'content': 'Third post content',
+        'date_posted': 'August 27, 2023'
+    },
+    {
+        'author': 'Virat Kohli',
+        'title': 'Blog Post 4',
+        'content': 'Fourth post content',
+        'date_posted': 'August 27, 2023'
+    }
+]
+
 
 def home(request):
-    return HttpResponse('<h1>Blog home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
 
 def about(request):
-    return HttpResponse('<h1>Blog about</h1>')
+    return render(request, 'blog/about.html',{'title': 'About'})
