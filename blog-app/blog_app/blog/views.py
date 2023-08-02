71bd1aa5af21644051f3455abd33aca6a3063f04
budhathoki356 <budhathoki1418@gmail.com>
2023-08-02 10:20:54 +0545

    feat: Database and migrations

--- blog-app/blog_app/blog/views.py
+++ blog-app/blog_app/blog/views.py
@@ -1,36 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Eklal Budhathoki',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2023'
-    },
-    {
-        'author': 'Aziz Hussen',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 27, 2023'
-    },
-    {
-        'author': 'Dhoni Sharma',
-        'title': 'Blog Post 3',
-        'content': 'Third post content',
-        'date_posted': 'August 27, 2023'
-    },
-    {
-        'author': 'Virat Kohli',
-        'title': 'Blog Post 4',
-        'content': 'Fourth post content',
-        'date_posted': 'August 27, 2023'
-    }
-]
-
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
